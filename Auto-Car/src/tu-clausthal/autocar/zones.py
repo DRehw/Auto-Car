0c53858b38e3698380601196e94d5ee51656a58f
Dave Rehwagen <dr17@tu-clausthal.de>
2019-05-22 11:29:18 +0200

    Changed variable and function names in zones module to PEP8 conform names.

--- Auto-Car/src/tu-clausthal/autocar/zones.py
+++ Auto-Car/src/tu-clausthal/autocar/zones.py
@@ -1,87 +1,90 @@
-'''
+"""
 Created on May 12, 2019
-
-@author: Dave
-'''
+"""
 
 import math
 import sensor
 from CurrentData import CurrentData
 
 
-def isObjectInRedZoneLidar(lidarData):
-    for dataset in lidarData:
-        if dataset[0] > 10 and (dataset[1] < 20.0 or dataset[1] > 340.0) and dataset[2]*math.cos(math.radians(dataset[1])) < 1000.0:
-            #print("{}, {}, {}".format(dataset[0], dataset[1], dataset[2]))
+def is_object_in_red_zone_lidar(lidar_data):
+    for dataset in lidar_data:
+        length_from_car_orientation = dataset[2]*math.cos(math.radians(dataset[1]))
+        if dataset[0] > 10 and (dataset[1] < 20.0 or dataset[1] > 340.0) and length_from_car_orientation < 1000.0:
+            # print("{}, {}, {}".format(dataset[0], dataset[1], dataset[2]))
             return True
     return False
 
-def isObjectInRedZoneUS():
-    usData = CurrentData.get_value_from_tag_from_sensor("us")
-    usData = usData[1:3]
-    for dist in usData:
-        if dist > 2 and dist < 100:
-            #print(str(usData) + "True")
+
+def is_object_in_red_zone_us():
+    us_data = CurrentData.get_value_from_tag_from_sensor("us")
+    us_data = us_data[1:3]
+    for dist in us_data:
+        if 2 < dist < 100:
+            # print(str(usData) + "True")
             return True
-    #print(str(usData) + "False")
+    # print(str(usData) + "False")
     return False
-def defineRedZoneDynamic(currentSpeed):
-    redZone = 100
-    maxSpeed = 12
-    minSpeed = 6
-    maxZone = 120
-    minZone = 50
-    speed = convertSpeed(currentSpeed)
 
-    if speed >= maxSpeed:
-        redZone = maxZone
-    elif speed <= minSpeed:
-        redZone = minZone
+
+def define_red_zone_dynamic(current_speed):
+    max_speed = 12
+    min_speed = 6
+    max_zone = 120
+    min_zone = 50
+    speed = convert_speed(current_speed)
+
+    if speed >= max_speed:
+        red_zone = max_zone
+    elif speed <= min_speed:
+        red_zone = min_zone
     else:
-        redZone = minZone + (maxZone - minZone)/((maxSpeed - minSpeed)/(speed - minSpeed))
-    #print(redZone)
-    return redZone
+        red_zone = min_zone + (max_zone - min_zone)/((max_speed - min_speed)/(speed - min_speed))
+    # print(redZone)
+    return red_zone
 
-def defineYellowZoneDynamic(currentSpeed):
-    redZone = 150
-    maxSpeed = 12
-    minSpeed = 6
-    maxZone = 300
-    minZone = 100
-    speed = convertSpeed(currentSpeed)
 
-    if speed >= maxSpeed:
-        redZone = maxZone
-    elif speed <= minSpeed:
-        redZone = minZone
+def define_yellow_zone_dynamic(current_speed):
+    max_speed = 12
+    min_speed = 6
+    max_zone = 300
+    min_zone = 100
+    speed = convert_speed(current_speed)
+
+    if speed >= max_speed:
+        red_zone = max_zone
+    elif speed <= min_speed:
+        red_zone = min_zone
     else:
-        redZone = minZone + (maxZone - minZone)/((maxSpeed - minSpeed)/(speed - minSpeed))
-    #print(redZone)
-    return redZone
+        red_zone = min_zone + (max_zone - min_zone)/((max_speed - min_speed)/(speed - min_speed))
+    # print(redZone)
+    return red_zone
 
-def isObjectInRedZoneUSDynamic(currentSpeed):
-    usData = CurrentData.get_value_from_tag_from_sensor("us")
-    usData = usData[1:3]
-    for dist in usData:
-        if dist > 2 and dist < defineRedZoneDynamic(currentSpeed):
-            #print(str(usData) + "True")
+
+def is_object_in_red_zone_us_dynamic(current_speed):
+    us_data = CurrentData.get_value_from_tag_from_sensor("us")
+    us_data = us_data[1:3]
+    for dist in us_data:
+        if 2 < dist < define_red_zone_dynamic(current_speed):
+            # print(str(usData) + "True")
             return True
         else:
-          return False
-    #print(str(usData) + "False")
+            return False
+    # print(str(usData) + "False")
 
 
-def isObjectInYellowZoneUSDynamic(currentSpeed):
-    usData = CurrentData.get_value_from_tag_from_sensor("us")
-    usData = usData[1:3]
-    for dist in usData:
-        if dist > 2 and dist < defineYellowZoneDynamic(currentSpeed):
-            #print(str(usData) + "True")
+def is_object_in_yellow_zone_us_dynamic(current_speed):
+    us_data = CurrentData.get_value_from_tag_from_sensor("us")
+    us_data = us_data[1:3]
+    for dist in us_data:
+        if 2 < dist < define_yellow_zone_dynamic(current_speed):
+            # print(str(usData) + "True")
             return True
         else:
-          return False
-    #print(str(usData) + "False")
+            return False
+    # print(str(usData) + "False")
 
-def convertSpeed(speed):
+
+def convert_speed(speed):
     speed = 15 - (speed - 75)
     return speed
